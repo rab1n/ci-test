9923902bccf23455583bf15386fbe416d51c8a59
nrccua-rabinj <rabin.joshi@nrccua.org>
2017-02-14 12:42:43 -0600

    adds project

---
+++ ContinuousIntegrationTest/ContinuousIntegrationTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ContinuousIntegrationTest
+//
+//  Created by Rabin Joshi on 2/14/17.
+//  Copyright © 2017 Rabin Joshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ContinuousIntegrationTest/ContinuousIntegrationTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContinuousIntegrationTest
+//
+//  Created by Rabin Joshi on 2/14/17.
+//  Copyright © 2017 Rabin Joshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
